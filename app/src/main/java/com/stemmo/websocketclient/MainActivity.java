592b05f7e740c41b6c759fde8b2d2a54151cad11
Stefano Mora <stefano.mora@newdep.com>
2019-02-27 17:53:50 +0100

    prima versione

---
+++ app/src/main/java/com/stemmo/websocketclient/MainActivity.java
@@ -0,0 +1,93 @@
+package com.stemmo.websocketclient;
+
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Applicativo per collegare un server websocket e generare una notifica.
+ *
+ */
+public class MainActivity extends Activity {
+
+    private WebSocketClient mWebSocketClient;
+    private TextView tvStatus;
+    private TextView tvMessages;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnConnect = findViewById(R.id.btnConnect);
+        Button btnDisconnect = findViewById(R.id.btnDisconnect);
+        Button btnSend = findViewById(R.id.btnSend);
+        tvStatus = findViewById(R.id.tvStatus);
+        tvMessages = findViewById(R.id.tvMessages);
+
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                connectWebSocket();
+            }
+        });
+    }
+
+
+
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("ws://websockethost:8080");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+
+                tvStatus.setText("Status: Connected");
+
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView textView = (TextView)findViewById(R.id.tvMessages);
+                        textView.setText(textView.getText() + "\n" + message);
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                tvStatus.setText("Status: Disconnected");
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
+}
