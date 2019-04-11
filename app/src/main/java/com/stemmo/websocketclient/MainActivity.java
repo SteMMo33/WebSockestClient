da34e6800f7bc28de314a9895ad14b67d4d4217b
Stefano M <stefano.mora@libero.it>
2019-04-11 18:28:46 +0200

    Aggiornata libreria alla 1.3.9 perchè la 1.3.0 ha versione IETF = 8 non gestita da ws server. Aggiunta gestione notifiche. Aggiunta pagina richiamata da notifica.

--- app/src/main/java/com/stemmo/websocketclient/MainActivity.java
+++ app/src/main/java/com/stemmo/websocketclient/MainActivity.java
@@ -1,6 +1,11 @@
 package com.stemmo.websocketclient;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +16,22 @@ import android.widget.TextView;
 
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
- * Applicativo per collegare un server websocket e generare una notifica.
+ * Applicativo per collegare un server websocket, gestire una risposta JSON e generare una notifica.
  *
  * Aggiunto     implementation "org.java-websocket:Java-WebSocket:1.3.0" nel file app\build.gradle
+ *
+ * Per sincronizzare GIT remoto ho dato il comando 'git pull origin master --allow-unrelated-histories'
+ *
+ * Notifiche: https://developer.android.com/guide/topics/ui/notifiers/notifications.html
+ * https://www.androidauthority.com/how-to-create-android-notifications-707254/
+ *
  */
 public class MainActivity extends Activity {
 
@@ -29,6 +42,7 @@ public class MainActivity extends Activity {
     Button btnConnect;
     Button btnDisconnect;
     Button btnSend;
+    private View thisView;
 
 
     @Override
@@ -46,7 +60,7 @@ public class MainActivity extends Activity {
         btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                connectWebSocket();
+                    connectWebSocket();
             }
         });
 
@@ -66,11 +80,15 @@ public class MainActivity extends Activity {
         });
         btnSend.setEnabled(false);
 
-        edtServer.setText("192.168.5.69");
+        edtServer.setText("192.168.5.169");
         tvMessages.setText("");
     }
 
 
+    private void AddToLog(String msg)
+    {
+        tvMessages.setText(tvMessages.getText() + msg);
+    }
 
     private void connectWebSocket() {
 
@@ -79,26 +97,44 @@ public class MainActivity extends Activity {
 
         URI uri;
         try {
-            uri = new URI("ws://"+ip);
+            uri = new URI("ws://"+ip+":7681");
         } catch (URISyntaxException e) {
             e.printStackTrace();
+            AddToLog(e.getMessage());
             return;
         }
 
+        // Creazione del websocket e definizione delle relative callbacks
         mWebSocketClient = new WebSocketClient(uri) {
+
+            /**
+             * Callback su apertura connessione
+             * @param serverHandshake
+             */
             @Override
             public void onOpen(ServerHandshake serverHandshake) {
 
-                btnConnect.setEnabled(false);
-                btnDisconnect.setEnabled(true);
-                btnSend.setEnabled(true);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        btnConnect.setEnabled(false);
+                        btnDisconnect.setEnabled(true);
+                        btnSend.setEnabled(true);
 
-                tvStatus.setText("Status: Connected");
 
-                Log.i("Websocket", "Opened");
-                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+                        AddToLog("Connected!\n");
+                        tvStatus.setText("Status: Connected");
+
+                        Log.i("Websocket", "Opened");
+                        mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+                    }
+                });
             }
 
+            /**
+             * Callback per la ricezione di un messaggio
+             * @param s
+             */
             @Override
             public void onMessage(String s) {
                 final String message = s;
@@ -107,10 +143,26 @@ public class MainActivity extends Activity {
                     public void run() {
                         TextView textView = (TextView)findViewById(R.id.tvMessages);
                         textView.setText(textView.getText() + "\n" + message);
+
+                        // Instantiate a JSON object from the request response
+                        try {
+                            JSONObject jsonObject = new JSONObject(message);
+                            jsonObject.getString("v");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 });
+
+                sendNotification(null);
             }
 
+            /**
+             * Callback sulla chiusura del websocket
+             * @param i
+             * @param s
+             * @param b
+             */
             @Override
             public void onClose(int i, String s, boolean b) {
                 Log.i("Websocket", "Closed: " + s);
@@ -118,6 +170,8 @@ public class MainActivity extends Activity {
                     @Override
                     public void run() {
                         tvStatus.setText("Status: Disconnected");
+                        AddToLog(".. disconnected");
+
                         btnConnect.setEnabled(true);
                         btnDisconnect.setEnabled(false);
                         btnSend.setEnabled(false);
@@ -132,7 +186,7 @@ public class MainActivity extends Activity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        tvMessages.setText(tvMessages.getText() + ex.getMessage());
+                        AddToLog(ex.getMessage());
                         tvStatus.setText("Status: Error");
 
                         btnConnect.setEnabled(true);
@@ -144,4 +198,44 @@ public class MainActivity extends Activity {
         };
         mWebSocketClient.connect();
     }
+
+    /**
+     * Funzione per generare una notifica
+     * @param view
+     */
+    public void sendNotification(View view) {
+
+        // prepare intent which is triggered if the
+        // notification is selected
+
+        Intent intent = new Intent(this, NotificationReceiver.class);
+        // use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+        // build notification
+        // the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("Notifica importante")
+                .setContentText("Notifica da applicazione websocket")
+                .setSmallIcon(R.drawable.icon)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.icon, "Call", pIntent)
+                .addAction(R.drawable.icon, "More", pIntent)
+                .addAction(R.drawable.icon, "And more", pIntent).build();
+
+        // Gets an instance of the NotificationManager service//
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // When you issue multiple notifications about the same type of event,
+        // it’s best practice for your app to try to update an existing notification
+        // with this new information, rather than immediately creating a new notification.
+        // If you want to update this notification at a later date, you need to assign it an ID.
+        // You can then use this ID whenever you issue a subsequent notification.
+        // If the previous notification is still visible, the system will update this existing notification,
+        // rather than create a new one. In this example, the notification’s ID is 001//
+        // NotificationManager.notify().
+        mNotificationManager.notify(001, n);
+    }
+
 }
